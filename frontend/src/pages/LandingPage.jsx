6629dc4be7178c02eba97b30fe5a0011b497e7f2
Aditya Tile <adityatile99@gmail.com>
2025-02-22 22:45:19 +0530

    create navbar component

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "../App.css";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div className="landingPageContainer">
+      <nav>
+        <div className="navHeader">
+          <h2>Streamify</h2>
+        </div>
+        <div className="navList">
+          <p>Join as Guest</p>
+          <p>Register</p>
+          <div role="button">
+            <p>Login</p>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default LandingPage;
