27d1c59e08aaf03000b45febf2ec06d2a16f1297
Aditya Tile <130766642+AdityaTile04@users.noreply.github.com>
2025-02-24 21:51:48 +0530

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -11,7 +11,6 @@ const LandingPage = () => {
         <div className="navList">
           <p>Join as Guest</p>
           <p>Register</p>
-
           
           <div role="button">
             <p>Login</p>
@@ -21,5 +20,6 @@ const LandingPage = () => {
     </div>
   );
 };
+
 
 export default LandingPage;
